6dcd008658728486e33396d869faf3ae9e1945d7
Biggo02 <o19549991@gmail.com>
2026-08-31 15:37:36 +0200

    feat: make production settings environment driven

--- fasthome/settings.py
+++ fasthome/settings.py
@@ -1,30 +1,19 @@
+import os
 from pathlib import Path
-
 BASE_DIR = Path(__file__).resolve().parent.parent
-SECRET_KEY = 'fasthome-dev-key-change-in-production'
-DEBUG = True
-ALLOWED_HOSTS = ['*']
-
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY','fasthome-dev-key-change-in-production')
+DEBUG = os.getenv('DJANGO_DEBUG','1') == '1'
+ALLOWED_HOSTS = [h.strip() for h in os.getenv('DJANGO_ALLOWED_HOSTS','*').split(',') if h.strip()]
 INSTALLED_APPS = ['django.contrib.admin','django.contrib.auth','django.contrib.contenttypes','django.contrib.sessions','django.contrib.messages','django.contrib.staticfiles','core']
 MIDDLEWARE = ['django.middleware.security.SecurityMiddleware','django.contrib.sessions.middleware.SessionMiddleware','django.middleware.common.CommonMiddleware','django.middleware.csrf.CsrfViewMiddleware','django.contrib.auth.middleware.AuthenticationMiddleware','django.contrib.messages.middleware.MessageMiddleware','django.middleware.clickjacking.XFrameOptionsMiddleware']
-ROOT_URLCONF = 'fasthome.urls'
-TEMPLATES = [{'BACKEND':'django.template.backends.django.DjangoTemplates','DIRS':[BASE_DIR/'templates'],'APP_DIRS':True,'OPTIONS':{'context_processors':['django.template.context_processors.debug','django.template.context_processors.request','django.contrib.auth.context_processors.auth','django.contrib.messages.context_processors.messages']}}]
-WSGI_APPLICATION = 'fasthome.wsgi.application'
-DATABASES = {'default': {'ENGINE':'django.db.backends.sqlite3','NAME':BASE_DIR/'db.sqlite3'}}
-AUTH_PASSWORD_VALIDATORS = [{'NAME':'django.contrib.auth.password_validation.MinimumLengthValidator','OPTIONS':{'min_length':8}}]
-LANGUAGE_CODE = 'fr-fr'
-TIME_ZONE = 'Africa/Lubumbashi'
-USE_I18N = True
-USE_TZ = True
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR/'static']
-STATIC_ROOT = BASE_DIR/'staticfiles'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR/'media'
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LOGIN_URL = '/connexion/'
-LOGIN_REDIRECT_URL = '/espace-personnel/'
-LOGOUT_REDIRECT_URL = '/'
-SESSION_COOKIE_HTTPONLY = True
-CSRF_COOKIE_HTTPONLY = True
-X_FRAME_OPTIONS = 'DENY'
+ROOT_URLCONF='fasthome.urls'; TEMPLATES=[{'BACKEND':'django.template.backends.django.DjangoTemplates','DIRS':[BASE_DIR/'templates'],'APP_DIRS':True,'OPTIONS':{'context_processors':['django.template.context_processors.debug','django.template.context_processors.request','django.contrib.auth.context_processors.auth','django.contrib.messages.context_processors.messages']}}]; WSGI_APPLICATION='fasthome.wsgi.application'
+if os.getenv('DATABASE_URL'):
+    import urllib.parse
+    u=urllib.parse.urlparse(os.getenv('DATABASE_URL')); DATABASES={'default':{'ENGINE':'django.db.backends.postgresql','NAME':u.path.lstrip('/'),'USER':u.username,'PASSWORD':u.password,'HOST':u.hostname,'PORT':u.port or 5432}}
+else: DATABASES={'default':{'ENGINE':'django.db.backends.sqlite3','NAME':BASE_DIR/'db.sqlite3'}}
+AUTH_PASSWORD_VALIDATORS=[{'NAME':'django.contrib.auth.password_validation.MinimumLengthValidator','OPTIONS':{'min_length':8}}]
+LANGUAGE_CODE='fr-fr'; TIME_ZONE='Africa/Lubumbashi'; USE_I18N=True; USE_TZ=True
+STATIC_URL='/static/'; STATICFILES_DIRS=[BASE_DIR/'static']; STATIC_ROOT=BASE_DIR/'staticfiles'; MEDIA_URL='/media/'; MEDIA_ROOT=BASE_DIR/'media'; DEFAULT_AUTO_FIELD='django.db.models.BigAutoField'
+LOGIN_URL='/connexion/'; LOGIN_REDIRECT_URL='/espace-personnel/'; LOGOUT_REDIRECT_URL='/'; SESSION_COOKIE_HTTPONLY=True; CSRF_COOKIE_HTTPONLY=True; X_FRAME_OPTIONS='DENY'
+if not DEBUG:
+    SESSION_COOKIE_SECURE=True; CSRF_COOKIE_SECURE=True; SECURE_PROXY_SSL_HEADER=('HTTP_X_FORWARDED_PROTO','https'); SECURE_HSTS_SECONDS=31536000; SECURE_HSTS_INCLUDE_SUBDOMAINS=True; SECURE_CONTENT_TYPE_NOSNIFF=True
