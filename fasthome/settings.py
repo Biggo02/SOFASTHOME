1851e14854b10ab48afe97b92d8fbcc1cb8d7581
Biggo02 <o19549991@gmail.com>
2026-08-31 15:32:46 +0200

    chore: harden Django development settings

--- fasthome/settings.py
+++ fasthome/settings.py
@@ -5,38 +5,26 @@ SECRET_KEY = 'fasthome-dev-key-change-in-production'
 DEBUG = True
 ALLOWED_HOSTS = ['*']
 
-INSTALLED_APPS = [
-    'django.contrib.admin','django.contrib.auth','django.contrib.contenttypes',
-    'django.contrib.sessions','django.contrib.messages','django.contrib.staticfiles',
-    'core',
-]
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware','django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware','django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware','django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+INSTALLED_APPS = ['django.contrib.admin','django.contrib.auth','django.contrib.contenttypes','django.contrib.sessions','django.contrib.messages','django.contrib.staticfiles','core']
+MIDDLEWARE = ['django.middleware.security.SecurityMiddleware','django.contrib.sessions.middleware.SessionMiddleware','django.middleware.common.CommonMiddleware','django.middleware.csrf.CsrfViewMiddleware','django.contrib.auth.middleware.AuthenticationMiddleware','django.contrib.messages.middleware.MessageMiddleware','django.middleware.clickjacking.XFrameOptionsMiddleware']
 ROOT_URLCONF = 'fasthome.urls'
-TEMPLATES = [{
-    'BACKEND':'django.template.backends.django.DjangoTemplates',
-    'DIRS':[BASE_DIR/'templates'], 'APP_DIRS':True,
-    'OPTIONS':{'context_processors':[
-        'django.template.context_processors.debug','django.template.context_processors.request',
-        'django.contrib.auth.context_processors.auth','django.contrib.messages.context_processors.messages',
-    ]},
-}]
+TEMPLATES = [{'BACKEND':'django.template.backends.django.DjangoTemplates','DIRS':[BASE_DIR/'templates'],'APP_DIRS':True,'OPTIONS':{'context_processors':['django.template.context_processors.debug','django.template.context_processors.request','django.contrib.auth.context_processors.auth','django.contrib.messages.context_processors.messages']}}]
 WSGI_APPLICATION = 'fasthome.wsgi.application'
 DATABASES = {'default': {'ENGINE':'django.db.backends.sqlite3','NAME':BASE_DIR/'db.sqlite3'}}
-AUTH_PASSWORD_VALIDATORS = []
+AUTH_PASSWORD_VALIDATORS = [{'NAME':'django.contrib.auth.password_validation.MinimumLengthValidator','OPTIONS':{'min_length':8}}]
 LANGUAGE_CODE = 'fr-fr'
 TIME_ZONE = 'Africa/Lubumbashi'
 USE_I18N = True
 USE_TZ = True
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR/'static']
-MEDIA_URL = 'media/'
+STATIC_ROOT = BASE_DIR/'staticfiles'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/connexion/'
 LOGIN_REDIRECT_URL = '/espace-personnel/'
 LOGOUT_REDIRECT_URL = '/'
+SESSION_COOKIE_HTTPONLY = True
+CSRF_COOKIE_HTTPONLY = True
+X_FRAME_OPTIONS = 'DENY'
