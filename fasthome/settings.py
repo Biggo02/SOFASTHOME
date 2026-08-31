75c062ce0eb5d3ad9030563530476126e6c9d64b
Biggo02 <o19549991@gmail.com>
2026-08-31 16:10:40 +0200

    feat: enable verified identity middleware

--- fasthome/settings.py
+++ fasthome/settings.py
@@ -5,7 +5,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY','fasthome-dev-key-change-in-productio
 DEBUG = os.getenv('DJANGO_DEBUG','1') == '1'
 ALLOWED_HOSTS = [h.strip() for h in os.getenv('DJANGO_ALLOWED_HOSTS','*').split(',') if h.strip()]
 INSTALLED_APPS = ['django.contrib.admin','django.contrib.auth','django.contrib.contenttypes','django.contrib.sessions','django.contrib.messages','django.contrib.staticfiles','core']
-MIDDLEWARE = ['django.middleware.security.SecurityMiddleware','django.contrib.sessions.middleware.SessionMiddleware','django.middleware.common.CommonMiddleware','django.middleware.csrf.CsrfViewMiddleware','django.contrib.auth.middleware.AuthenticationMiddleware','django.contrib.messages.middleware.MessageMiddleware','django.middleware.clickjacking.XFrameOptionsMiddleware']
+MIDDLEWARE = ['django.middleware.security.SecurityMiddleware','django.contrib.sessions.middleware.SessionMiddleware','django.middleware.common.CommonMiddleware','django.middleware.csrf.CsrfViewMiddleware','django.contrib.auth.middleware.AuthenticationMiddleware','django.contrib.messages.middleware.MessageMiddleware','django.middleware.clickjacking.XFrameOptionsMiddleware','core.middleware.VerifiedIdentityMiddleware']
 ROOT_URLCONF='fasthome.urls'; TEMPLATES=[{'BACKEND':'django.template.backends.django.DjangoTemplates','DIRS':[BASE_DIR/'templates'],'APP_DIRS':True,'OPTIONS':{'context_processors':['django.template.context_processors.debug','django.template.context_processors.request','django.contrib.auth.context_processors.auth','django.contrib.messages.context_processors.messages']}}]; WSGI_APPLICATION='fasthome.wsgi.application'
 if os.getenv('DATABASE_URL'):
     import urllib.parse
