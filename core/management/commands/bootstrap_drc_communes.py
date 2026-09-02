4ed3fd8dae777090ba705d91006d4cab69781122
Biggo02 <o19549991@gmail.com>
2026-09-02 19:30:13 +0200

    Fix bootstrap_drc_communes wrapper after importer cleanup

--- core/management/commands/bootstrap_drc_communes.py
+++ core/management/commands/bootstrap_drc_communes.py
@@ -1,5 +1,4 @@
 """Point d'entrée historique de l'import géographique FASTHOME."""
 
-# OSM est utilisé en priorité, puis geoBoundaries complète les communes
-# du référentiel FASTHOME dont OSM ne fournit pas la géométrie exploitable.
-from .bootstrap_fasthome_communes_external import Command
+# Compatibilité: le vrai importeur est désormais bootstrap_fasthome_communes.
+from .bootstrap_fasthome_communes import Command
