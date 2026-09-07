5e3fc84ac3c00f88e81c13212a0ab2645c26ec6a
Biggo02 <o19549991@gmail.com>
2026-09-07 12:34:06 +0200

    Contrats: activer le générateur PDF professionnel

--- core/apps.py
+++ core/apps.py
@@ -1,4 +1,9 @@
 from django.apps import AppConfig
+
+
 class CoreConfig(AppConfig):
-    default_auto_field='django.db.models.BigAutoField'
-    name='core'
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'core'
+
+    def ready(self):
+        from . import rental_document_signals  # noqa: F401
