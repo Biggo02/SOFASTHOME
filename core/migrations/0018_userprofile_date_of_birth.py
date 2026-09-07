2642e6f97366737f9589c0b6d75ce39e88e5654d
Biggo02 <o19549991@gmail.com>
2026-09-07 19:19:27 +0200

    Fix migration dependency for user profile birth date

--- core/migrations/0018_userprofile_date_of_birth.py
+++ core/migrations/0018_userprofile_date_of_birth.py
@@ -3,7 +3,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ('core', '0017_rentalcase_financial_fields'),
+        ('core', '0018_userprofile'),
     ]
 
     operations = [
