e38303cabf5c73d8c2f44953c11277c8c716c165
Biggo02 <o19549991@gmail.com>
2026-09-07 19:10:47 +0200

    feat: add user profile migration

---
+++ core/migrations/0018_userprofile.py
@@ -0,0 +1,36 @@
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('core', '0017_rentalcase_financial_fields'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='UserProfile',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('postnom', models.CharField(blank=True, max_length=100)),
+                ('profession', models.CharField(blank=True, choices=[
+                    ('agent_immobilier', 'Agent immobilier'),
+                    ('agriculteur', 'Agriculteur'),
+                    ('artisan', 'Artisan'),
+                    ('commercant', 'Commerçant'),
+                    ('enseignant', 'Enseignant'),
+                    ('fonctionnaire', 'Fonctionnaire'),
+                    ('medecin', 'Médecin / professionnel de santé'),
+                    ('ingenieur', 'Ingénieur'),
+                    ('juriste', 'Juriste / avocat'),
+                    ('comptable', 'Comptable / financier'),
+                    ('entrepreneur', 'Entrepreneur'),
+                    ('etudiant', 'Étudiant'),
+                    ('sans_emploi', 'Sans emploi'),
+                    ('retraite', 'Retraité'),
+                    ('autre', 'Autre'),
+                ], max_length=40)),
+                ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, related_name='profile', to='auth.user')),
+            ],
+        ),
+    ]
