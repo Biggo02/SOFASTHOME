00b10bfbcf6eb19564acf8c8dda72556ab776329
Biggo02 <o19549991@gmail.com>
2026-09-07 10:28:38 +0200

    Fusionner les branches de migrations de location

---
+++ core/migrations/0015_merge_rental_documents.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('core', '0014_rental_documents_four_way'),
+        ('core', '0016_alter_rentalcase_status'),
+    ]
+
+    operations = []
